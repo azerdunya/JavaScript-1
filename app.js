6363ec2737925e6c7a595fa58571291de635717c
Azer Dunyamaliyev <azerdunya2005@gmail.com>
2023-03-22 11:53:37 +0400

    JavaScript one day

---
+++ app.js
@@ -0,0 +1,51 @@
+// var a = 729+(-5126)+(-629)+5126;
+// console.log(a);
+
+
+// var a = -638+(-178)+538+173;
+// console.log(a);
+
+
+// var a = -372+984+172+(-2084);
+// console.log(a);
+
+// var a = -78+36+19+(-22)+(-25);
+// console.log(a);
+
+// var a = 125/(-25)+100*(-2);
+// console.log(a);
+
+// var a = 745/(-5)-42/(-21);
+// console.log(a);
+
+// var a = 90*(-3)/27+12;
+// console.log(a);
+
+// var a = -1200/(-60)-64/(-8);
+// console.log(a);
+
+// var b = 9;
+// var a = 2+(b-4);
+// console.log(a);
+
+// var y = 6;
+// var a = -(3-y)+7;
+// console.log(a);
+
+// var x = 20;
+// var y = 90;
+// var a = 2*(x+y)-2*x;
+// console.log(a);
+
+// var y = 50;
+// var a = (3+y)-42;
+// console.log(a);
+
+// var x = 70;
+// var a = (x-5)+(x+7);
+// console.log(a);
+
+// var a = 10;
+// var b = 30;
+// var c = 4*(a-2*b)+8*b;
+// console.log(c);
